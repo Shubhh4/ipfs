add4f826f4d56ca5e4e14e6c8d9538b69b38d342
Shubham Pathak <95904081+Shubhh4@users.noreply.github.com>
2023-03-29 22:49:00 +0530

    Update Upload.sol

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -6,7 +6,7 @@ contract Upload {
   
   struct Access{
      address user; 
-     bool access; //true or sfalse
+     bool access; //true or false
   }
   mapping(address=>string[]) value;
   mapping(address=>mapping(address=>bool)) ownership;
